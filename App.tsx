abbd79f80f9997c82e5c4e3945abc481a3dc2fbf
Willian S <wsapcf@gmail.com>
2021-05-22 07:57:29 -0300

    refactor Welcome.tsx to match design

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Welcome } from './src/pages/Welcome';
 import {useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost'
+import AppLoading from 'expo-app-loading'
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular, Jost_600SemiBold
   })
-  if(fontsLoaded)
+  if(!fontsLoaded)
+    return <AppLoading/>
   return (
       <Welcome/>
   );
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -3,36 +3,48 @@ import {Text, View, Image, TouchableOpacity, StyleSheet, SafeAreaView, Dimension
 import watering from '../assets/watering.png'
 import { Feather } from '@expo/vector-icons'
 import colors from '../styles/colors'
+import fonts from '../styles/fonts'
 export function Welcome(){
   return (
     <SafeAreaView style={styles.container} >  
-      <Text style={styles.title}  >Gerencie {'\n' } suas Plantas {'\n' } de forma fácil </Text>
+     <View style={styles.wrapper} >
+        <Text style={styles.title}  >Gerencie {'\n' } suas Plantas {'\n' } de forma fácil </Text>
       <Image source={watering} style={styles.image} resizeMode="contain" />
       <Text style={styles.subtitle} >Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você, sempre que precisar</Text>
         <TouchableOpacity style={styles.button} activeOpacity={0.7} >
           <Feather name="chevron-right" style={styles.buttonIcon} />
       </TouchableOpacity>
+     </View>
    </SafeAreaView> 
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
+
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 30
   },
   subtitle:{
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
 
   },
  
