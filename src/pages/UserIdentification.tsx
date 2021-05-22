5ea843a1b0b3dd88acb1fb8d5faf35d6680553a0
Willian S <wsapcf@gmail.com>
2021-05-22 08:33:51 -0300

    make form reactive

--- src/pages/UserIdentification.tsx
+++ src/pages/UserIdentification.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { View, Text, TextInput, SafeAreaView, StyleSheet, KeyboardAvoidingView, Platform} from 'react-native'
 import { Button } from '../components/Button'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
+
 export function UserIdentification(){
+
+  const [isFocused, setIsFocused] = useState(false)
+  const [isFilled, setIsFilled] = useState(false)
+  const [name, setName] = useState<string>()
+
+  function handleInputBlur(){
+    setIsFocused(false)
+    setIsFilled(!!name)
+  }
+  function handleInputFocus(){
+    setIsFocused(true)
+  }
+  function handleInputChange(value:string){
+    setIsFilled(!!value)
+    setName(value)
+  }
   return(
     <SafeAreaView style={styles.container} >
       <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}  >
         <View style={styles.wrapper} >
         <View style={styles.form}>
-           <Text style={styles.emoji} >😄</Text>
+           <Text style={styles.emoji} >{isFilled ? '😄' : '🙃'}</Text>
         <Text style={styles.title} >Como podemos{'\n'} chamar você?</Text>
-        <TextInput style={styles.input} placeholder="Digite um nome"  />
+        <TextInput style={[styles.input, (isFocused || isFilled) && {borderColor:colors.green}]} 
+        placeholder="Digite um nome" 
+        onBlur={handleInputBlur} 
+        onFocus={handleInputFocus}  
+        onChangeText={handleInputChange}
+        
+        />
         <View style={styles.footer}>
                   <Button title="Confirmar" />
 
