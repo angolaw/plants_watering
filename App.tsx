19807d766aeec7c241972047defc19f282008395
Willian S <wsapcf@gmail.com>
2021-05-22 07:20:50 -0300

    refactor App.tsx page

--- App.tsx
+++ App.tsx
@@ -5,17 +5,8 @@ import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
   return (
-    <View style={styles.container}>
       <Welcome/>
-    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
