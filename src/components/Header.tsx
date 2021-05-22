f9926215c78a1d4d34afc3dfbfd3f18ace766ade
Willian S <wsapcf@gmail.com>
2021-05-22 10:13:32 -0300

    create Header component and config title and subtitle texts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Text, View, StyleSheet, SafeAreaView, Image } from 'react-native'
 import colors from '../styles/colors'
 import {getStatusBarHeight} from 'react-native-iphone-x-helper'
-
+import me from '../assets/will.jpeg'
+import fonts from '../styles/fonts'
 export function Header(){
   return (
     <View style={styles.container} >
         <View>
-          <Text>Olá,</Text>
-          <Text>Willian</Text>
+          <Text style={styles.greeting} >Olá,</Text>
+          <Text  style={styles.userName}>Willian</Text>
         </View>
+        <Image source={me} resizeMode="contain" style={styles.image} />
     </View>
   )
 }
@@ -22,8 +24,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingVertical: 20,
     marginTop: getStatusBarHeight(),
+  },
+ 
+  image: {
+    height: 70,
+    width: 70,
+    borderRadius: 40,
 
-
-
-  }
+  },
+   greeting: {
+     fontFamily: fonts.text,
+     fontSize: 32,
+     color: colors.heading,
+     
+   },
+   userName:{
+     fontFamily: fonts.heading,
+     fontSize: 32,
+     color: colors.heading,
+     lineHeight: 40,
+   }
 })
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -7,8 +7,11 @@ import fonts from '../styles/fonts'
 export function PlantSelect(){
   return (
     <View style={styles.container} >
-     <Header/>
-      <Text>Selecionar Planta</Text>
+      <View style={styles.header} >
+         <Header/>
+      <Text style={styles.title}>Em qual ambiente</Text>
+      <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
     </View>
   )
 }
@@ -16,8 +19,24 @@ export function PlantSelect(){
 const styles = StyleSheet.create({
   container:{
     flex: 1,
-
     backgroundColor: colors.background
   },
+  title: {
+    color: colors.heading,
+    fontSize: 17,
+    fontFamily: fonts.heading,
+    lineHeight:20,
+    marginTop:15
+
+  },
+   header:{
+      paddingHorizontal: 30,
+  },
+  subtitle:{
+    color: colors.body_light,
+    fontSize: 17,
+    fontFamily: fonts.text,
+    lineHeight: 20,
+  }
   
 })
