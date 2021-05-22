2ebd65a112855f43037c29977471f3cfaf3c84d6
Willian S <wsapcf@gmail.com>
2021-05-22 07:20:17 -0300

    finish welcome page setup

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,12 +1,54 @@
 import React from 'react'
-import {Text, View, Image} from 'react-native'
+import {Text, View, Image, TouchableOpacity, StyleSheet, SafeAreaView} from 'react-native'
 import watering from '../assets/watering.png'
+import colors from '../styles/colors'
 export function Welcome(){
   return (
-    <View>  
-      <Text>Gerencie suas Plantas de forma fácil </Text>
-      <Image source={watering} />
-      <Text>Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você, sempre que precisar</Text>
-   </View> 
+    <SafeAreaView style={styles.container} >  
+      <Text style={styles.title}  >Gerencie {'\n' } suas Plantas {'\n' } de forma fácil </Text>
+      <Image source={watering} style={styles.image} />
+      <Text style={styles.subtitle} >Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você, sempre que precisar</Text>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.buttonText} > > </Text>
+      </TouchableOpacity>
+   </SafeAreaView> 
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38
+  },
+  subtitle:{
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    color: colors.heading
+
+  },
+  button:{
+    backgroundColor: colors.green,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height:56,width:56,
+  },
+  image: {
+    width:292,
+    height:284,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
+
+  }
+})
