62819502447b2316a7368391aea911c0253420bf
Willian S <wsapcf@gmail.com>
2021-05-22 10:43:58 -0300

    call api to get plants environments

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Text, SafeAreaView, StyleSheet, View , FlatList} from 'react-native'
 import { EnvironmentButton } from '../components/EnvironmentButton'
 import { Header } from '../components/Header'
+import api from '../services/api'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
 
+interface EnvironmentProps{
+  key: string;
+  title: string;
+}
+
 export function PlantSelect(){
+  const [environments, setEnvironments] = useState<EnvironmentProps[]>([])
+  useEffect(() =>{
+    async function fetchEnvironment(){
+      const  {data} = await api.get('plants_environments')
+      setEnvironments([{key: 'all',title: 'Todos'},...data])
+    }
+    fetchEnvironment()
+  },[])
+  
+
   return (
     <View style={styles.container} >
       <View style={styles.header} >
@@ -15,8 +31,8 @@ export function PlantSelect(){
       </View>
       <View>
           <FlatList
-            data={[1,2,3,4,5]}
-            renderItem={({item}) => (<EnvironmentButton title={item} />)}
+            data={environments}
+            renderItem={({item}) => (<EnvironmentButton title={item.title} />)}
             horizontal
             showsHorizontalScrollIndicator={false}
             contentContainerStyle={styles.environmentList}
