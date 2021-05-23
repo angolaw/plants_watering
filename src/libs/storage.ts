06158c18655a000304932244622004707eb7d8da
Willian S <wsapcf@gmail.com>
2021-05-23 11:08:16 -0300

    add expo notifications;'

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -23,6 +23,22 @@ export interface StoragePlantProps {
 }
 export async function savePlant(plant: PlantProps): Promise<void>{
   try{
+    const nextTime = new Date(plant.dateTimeNotification)
+    const now = new Date()
+    const {times,repeat_every} = plant.frequency
+
+    if(repeat_every ==='week'){
+      const interval = Math.trunc(7/times)
+      nextTime.setDate(now.getDate() + interval)
+    }else{
+      nextTime.setDate(nextTime.getDate()+1)
+    }
+
+    const seconds = Math.abs(
+      Math.ceil(now.getTime() - nextTime.getTime()/ 1000)
+    )
+
+
     const data = await AsyncStorage.getItem('@plantmanager:plants')
     const oldPlants = data ? (JSON.parse(data) as StoragePlantProps): {}
     const newPlant = {
